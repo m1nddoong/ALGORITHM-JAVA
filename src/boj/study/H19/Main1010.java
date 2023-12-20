c1103d228d2b9c35929c578d8a1890a638e87e7a
m1nddoong <alstjsdlr0321@korea.ac.kr>
2023-12-20 22:04:25 +0900

    12/20 algo

---
+++ src/boj/study/H19/Main1010.java
@@ -0,0 +1,22 @@
+package boj.study.H19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < T; i++) {
+            String Input = reader.readLine();
+            int N = Integer.parseInt(Input.split(" ")[0]);
+            int M = Integer.parseInt(Input.split(" ")[1]);
+
+        }
+
+    }
+
+
+}
