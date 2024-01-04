f91e2f3923b314ca559a050fa37a1b1c74037b26
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-01-04 20:27:06 +0900

    1/4

--- src/boj/study/H22/Main14888.java
+++ src/boj/study/H22/Main14888.java
@@ -17,6 +17,7 @@ public class Main14888 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 수의 개수 N
         N = Integer.parseInt(br.readLine());
         number = new int[N];
 
@@ -26,50 +27,16 @@ public class Main14888 {
             number[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 연산자 입력
+        // N-1 개의 연산자 입력
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < 4; i++) {
             operator[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(number[0], 1);
+        for (int i = 0; i < N-1; i++) {
 
-        System.out.println(MAX);
-        System.out.println(MIN);
+        }
 
     }
 
-    public static void dfs(int num, int idx) {
-        if (idx == N) {
-            MAX = Math.max(MAX, num);
-            MIN = Math.min(MIN, num);
-            return;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            // 연산자 개수가 1개 이상인 경우
-            if (operator[i] > 0) {
-
-                // 해당 연산자를 1 감소시킨다.
-                operator[i]--;
-
-                switch (i) {
-                    case 0:
-                        dfs(num + number[idx], idx + 1);
-                        break;
-                    case 1:
-                        dfs(num - number[idx], idx + 1);
-                        break;
-                    case 2:
-                        dfs(num * number[idx], idx + 1);
-                        break;
-                    case 3:
-                        dfs(num / number[idx], idx + 1);
-                        break;
-                }
-                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
-                operator[i]++;
-            }
-        }
-    }
 }
