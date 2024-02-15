d9c23440cccf9e763688b12f8522700a76a3f35f
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-02-15 19:48:14 +0900

    2/15 문제풀이

--- src/boj/study/H6/Main1157.java
+++ src/boj/study/H6/Main1157.java
@@ -1,2 +1,8 @@
-package boj.study.H6;public class Main1157 {
+package boj.study.H6;
+
+// 단어 공부 : https://www.acmicpc.net/problem/1157
+public class Main1157 {
+    public static void main(String[] args) {
+
+    }
 }
