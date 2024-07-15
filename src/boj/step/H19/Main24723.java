cd2f20d48d0019bddc848e58c2fbd13c90cc1815
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-07-15 14:12:49 +0900

    24/07/15 - 녹색거탑

---
+++ src/boj/step/H19/Main24723.java
@@ -0,0 +1,18 @@
+package boj.step.H19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main24723 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 1;
+        for (int i = 0; i < N; i++) {
+            sum *= 2;
+        }
+        System.out.println(sum);
+    }
+}
