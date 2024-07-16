debd93b9758ff2064fa543a2d8d5a0ff1f88e829
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-07-16 18:35:22 +0900

    24/07/16 - 이항계수 1 - BC

--- src/boj/step/H19/Main11050.java
+++ src/boj/step/H19/Main11050.java
@@ -13,13 +13,25 @@ public class Main11050 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+        // System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+        System.out.println(BC(N, K));
     }
 
+    // 팩토리얼
     public static int factorial(int N) {
         if (N <= 1) { // factorial(0) == 1
             return 1;
         }
         return N * factorial(N - 1);
+    }
+
+    // 이항계수의 성질
+    public static int BC(int n, int k) {
+        // 2번 성질
+        if (n == k || k == 0) {
+            return 1;
+        }
+        // 1번 성질
+        return BC(n-1, k-1) + BC(n-1, k);
     }
 }
