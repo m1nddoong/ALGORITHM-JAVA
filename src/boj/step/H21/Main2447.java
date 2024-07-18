fbd9920693e447e78c0205809570004457fbfaf3
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-07-18 15:04:36 +0900

    24/07/18 - 재귀 - 별 찍기 - 10

--- src/boj/step/H21/Main2447.java
+++ src/boj/step/H21/Main2447.java
@@ -5,13 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main2447 {
-    public static int[][] arr;
+    public static char[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        arr = new int[N][N];
+        arr = new char[N][N];
         star(0, 0, N, false);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
     }
 
     public static void star (int x, int y, int N, boolean blank) {
