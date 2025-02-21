9d17c2c4e4ebdc6e32e2b10b25716f71d3e529b4
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-02-21 12:40:25 +0900

    [25/02/21]

--- src/programmers/알고리즘_고득점_Kit/N01_해시/Solution42577.java
+++ src/programmers/알고리즘_고득점_Kit/N01_해시/Solution42577.java
@@ -1,16 +1,43 @@
 package programmers.알고리즘_고득점_Kit.N01_해시;
 
+import java.util.*;
+
 // 전화번호 목록 : https://school.programmers.co.kr/learn/courses/30/lessons/42577
 public class Solution42577 {
     public static boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+        /*
+        해시 풀이 X
+         */
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String s = phone_book[i];
+            if (phone_book[i + 1].startsWith(s)) {
+                return false;
+            }
+        }
+         return true;
+
+        /* 이전 풀이
+        for (String a : phone_book) {
+            for (String b : phone_book) {
+                if (!a.equals(b) && b.startsWith(a)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+        */
     }
 
     public static void main(String[] args) {
         String[] phone_book1 = {"119", "97674223", "1195524421"};
         String[] phone_book2 = {"123","456","789"};
         String[] phone_book3 = {"12","123","1235","567","88"};
+        System.out.println(solution(phone_book1));
+        System.out.println(solution(phone_book2));
+        System.out.println(solution(phone_book3));
+
     }
 }
+
 
