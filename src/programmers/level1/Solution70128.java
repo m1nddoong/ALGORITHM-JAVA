d129d679f03696959011e916a657ea197ba4a3c6
Iksun <110027583+m1nddoong@users.noreply.github.com>
2025-03-22 00:36:19 +0900

    [25/03/22] Programmers level 1

--- src/programmers/level1/Solution70128.java
+++ src/programmers/level1/Solution70128.java
@@ -3,7 +3,13 @@ package programmers.level1;
 // 내적 : https://school.programmers.co.kr/learn/courses/30/lessons/70128
 public class Solution70128 {
     public int solution(int[] a, int[] b) {
+        int answer = 0;
 
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
