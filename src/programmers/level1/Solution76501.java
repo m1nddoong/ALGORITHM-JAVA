9c6abfa2b11d112d99a9591123c0cb3813b7b456
Iksun <110027583+m1nddoong@users.noreply.github.com>
2025-03-21 16:34:16 +0900

    [25/03/21] Programmers level 1

--- src/programmers/level1/Solution76501.java
+++ src/programmers/level1/Solution76501.java
@@ -3,7 +3,18 @@ package programmers.level1;
 // 음양 더하기 : https://school.programmers.co.kr/learn/courses/30/lessons/76501
 public class Solution76501 {
     public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
 
+        for (int i = 0; i < absolutes.length; i++) {
+            if (!signs[i]) {
+                answer += absolutes[i] - 2 * absolutes[i];
+
+            } else {
+                answer += absolutes[i];
+            }
+        }
+        
+        return answer;
     }
 
     public static void main(String[] args) {
